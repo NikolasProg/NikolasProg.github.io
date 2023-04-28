f04a78574f7a9f9a7cb673701427364d70df5f3b
NikolasProg <129743960+NikolasProg@users.noreply.github.com>
2023-04-28 23:05:08 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let btn1 = document.getElementById("btn1");
 let order = document.getElementById("order");
 
 btn1.addEventListener("click", () =>{
-    document.getElementById("main").style.display = "none";
+    document.getElementById("container").style.display = "none";
     document.getElementById("form").style.display = "block";
     document.getElementById("user_name").value = tg.initDataUnsafe.first_name + " "+ tg.initDataUnsafe.last_name;
 });
